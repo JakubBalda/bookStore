80523db48ad391a527719cf4eab031b3082b7691
JakubBalda <balda.jakub00@gmail.com>
2022-08-11 13:42:14 +0200

    adding books router

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,37 @@ const router = express.Router();
 
 /* GET books index /books */
 router.get('/', function(req, res, next) {
-    res.send('/books');
+    res.send('INDEX /books');
+});
+
+/* GET books new /books/new */
+router.get('/new', function(req, res, next) {
+    res.send('NEW /books/new');
+});
+
+/* POST books create /books */
+router.post('/', function(req, res, next) {
+    res.send('CREATE /books');
+});
+
+/* GET books show /books/:id */
+router.get('/:id', function(req, res, next) {
+    res.send('SHOW /books/:id');
+});
+
+/* GET books edit /books/:id/edit */
+router.get('/:id/edit', function(req, res, next) {
+    res.send('EDIT /books/:id/edit');
+});
+
+/* PUT books update /books/:id */
+router.put('/:id', function(req, res, next) {
+    res.send('UPDATE /books/:id');
+});
+
+/* DELETE books destroy /books/:id */
+router.delete('/:id', function(req, res, next) {
+    res.send('DESTROY /books/:id');
 });
 
 module.exports = router;
