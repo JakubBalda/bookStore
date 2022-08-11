4b09ae73db3213ed91a880c4efc061cebc44b5b7
JakubBalda <balda.jakub00@gmail.com>
2022-08-11 14:26:21 +0200

    add orders and users routes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const booksRouter = require('./routes/books');
+const ordersRouter = require('./routes/orders');
 
 const app = express();
 
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
-app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,54 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET /register */
+router.get('/register', function(req, res, next) {
+  res.send('GET /register')
+});
+
+/* POST /register */
+router.post('/register', function(req, res, next) {
+  res.send('POST /register')
+});
+
+/* GET /login */
+router.get('/login', function(req, res, next) {
+  res.send('GET /login')
+});
+
+/* POST /login */
+router.post('/login', function(req, res, next) {
+  res.send('POST /login')
+});
+
+/* GET /login */
+router.get('/profile', function(req, res, next) {
+  res.send('GET /profile')
+});
+
+/* PUT /login/:user_id */
+router.put('/profile:/user_id', function(req, res, next) {
+  res.send('GET /profile/:user_id')
+});
+
+/* GET /forgot-pw */
+router.get('/forgot-pw', function(req, res, next) {
+  res.send('GET /forgot-pw')
+});
+
+/* PUT /forgot-pw */
+router.put('/forgot-pw', function(req, res, next) {
+  res.send('PUT /forgot-pw')
+});
+
+/* GET /reset-pw/:token */
+router.get('/reset-pw/:token', function(req, res, next) {
+  res.send('GET /reset-pw/:token')
+});
+
+/* PUT /reset-pw/:token */
+router.put('/reset-pw/:token', function(req, res, next) {
+  res.send('PUT /reset-pw/:token')
 });
 
 module.exports = router;
