f18334860c884de23b628d58c496c1ba0d6fc71f
Jakub Balda <kuber579@gmail.com>
2022-08-25 10:45:13 +0200

    logging and renaming files and functions

--- Repositories/inmemory.js
+++ Repositories/inmemory.js
@@ -1,26 +1,38 @@
-    const parser = require('../middleware/xmlParser');
-    const logger = require('../middleware/logger');
-    const mapper = require('../middleware/mapper');
-    var booksArray;
-    
-    function readBooks(){
-        loadData();
-            
-        return mapper.mapCollection(booksArray);
+const parser = require('../middleware/xmlParser');
+const logger = require('../middleware/logger');
+const mapper = require('../middleware/mapper');
+var booksArray;
+
+//TODO: logging
+function readBooks(){
+    logger.logInformation('readBooks from repository requested');
+    loadData();
+        
+    let result =  mapper.mapCollection(booksArray);
+
+    logger.logData(result);
+    return result;
+}
+
+function readBookById(bookId){
+    logger.logInformation('readBookById from repository requested');
+    loadData();
+
+    let book = booksArray[bookId-1];
+    let bookWithDetails = mapper.mapSingle(book);;
+
+    logger.logData(bookWithDetails);
+    return bookWithDetails;
+}
+
+function loadData(){
+    logger.startTask('loadData');
+
+    if(booksArray === null || booksArray === undefined){
+        booksArray = parser.parseXml()[0];
     }
 
-    function readBookById(bookId){
-        loadData();
-
-        let book = booksArray[bookId-1];
-
-        return mapper.mapSingle(book);
-    }
-
-    function loadData(){
-        if(booksArray === null || booksArray === undefined){
-            booksArray = parser.parseXml()[0];
-        }
-    }
+    logger.endTask('loadData');
+}
 
 module.exports = {readBooks, readBookById};
