3a7e7c3bd24150402b9882ef07f0db17d2b03e47
Jakub Balda <kuber579@gmail.com>
2022-08-25 10:54:39 +0200

    logging and renaming files and functions

--- services/getBookDetailsUseCase.js
+++ services/getBookDetailsUseCase.js
@@ -1,9 +1,9 @@
-const inmemoryRepository = require('../Repositories/inmemory');
+const repository = require('../Repositories/inmemory');
 const logger = require('../middleware/logger');
 
 function getBookDetails(id){
     logger.startTask('book detalis');
-    let bookDetails = inmemoryRepository.readBookById(id);
+    let bookDetails = repository.readBookById(id);
     
     return bookDetails; 
 }
