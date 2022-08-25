9230cefdcd03610dbf1762fa9813f9f26f48c601
Jakub Balda <kuber579@gmail.com>
2022-08-25 15:41:33 +0200

    added logging and created database.js file

--- services/getBookDetailsUseCase.js
+++ services/getBookDetailsUseCase.js
@@ -2,9 +2,10 @@ const repository = require('../Repositories/inmemory');
 const logger = require('../middleware/logger');
 
 function getBookDetails(id){
-    logger.startTask('book detalis');
+    logger.logInformation('getAllBooksUseCase.getBooks requested');
     let bookDetails = repository.readBookById(id);
     
+    logger.logData(bookDetails);
     return bookDetails; 
 }
 
