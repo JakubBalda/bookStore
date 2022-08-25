9867b62b4cee2f3e5ebfe446d36d1ad8d20d0fe6
Jakub Balda <kuber579@gmail.com>
2022-08-25 15:42:40 +0200

    added sql query executor

--- database.js
+++ database.js
@@ -6,3 +6,22 @@ let databaseConnection = mysql.createConnection({
     password: '',
     database: 'bookStore'
 });
+
+async function sqlQuery(query){
+    
+    databaseConnection.connect((err) => {
+        if(err) throw err;  
+        console.log("Connected successfuly!");
+    });
+
+    let data;
+    
+    const queryExecute = util.promisify(databaseConnection.query).bind(databaseConnection);
+
+    data = await queryExecute(query);
+
+    databaseConnection.end();
+    return data;
+}
+
+module.exports = {sqlQuery};
