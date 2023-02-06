29e7c227e2b14750f54a65f76f28ebc1eb215d13
Jakub Bałda <balda.jakub00@gmail.com>
2023-02-06 12:07:10 +0100

    added unit tests

---
+++ test/booleanFunctions.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { expect } = require('chai');
+const booleanFunctions = require('../utils/booleanFunctions');
+
+describe('booleanFunctions.js tests', ()=>{
+    describe('booleanFunctions.isNullOrUndefined() Test', () =>{
+        it('should be true(undefined)', () =>{
+            let testObject = undefined;
+            const result = booleanFunctions.isNullOrUndefined(testObject);
+            expect(result).to.be.true;
+        });
+
+        it('should be true(null)', () =>{
+            let testObject = null;
+            const result = booleanFunctions.isNullOrUndefined(testObject);
+            expect(result).to.be.true;
+        });
+
+        it('should be false', () =>{
+            let testObject = "null";
+            const result = booleanFunctions.isNullOrUndefined(testObject);
+            expect(result).to.be.false;
+        });
+    });
+});
