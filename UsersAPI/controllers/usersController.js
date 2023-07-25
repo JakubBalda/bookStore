693a5c815086c5272b4da2653228362050743b50
Jakub Balda <balda.jakub00@gmail.com>
2023-07-25 16:22:13 +0200

    Added cors request handling

--- UsersAPI/controllers/usersController.js
+++ UsersAPI/controllers/usersController.js
@@ -6,10 +6,10 @@ const userRegisterUseCase = require('../services/userRegisterUseCase');
 async function login(req, res, next){
     logRequest('login', req);
 
-    let userLoginData = mapper.mapRequestToUserLoginModel(req.query);
-    let isLoggingInPassed = await userLoginUseCase.userLogginIn(userLoginData);
+    //let userLoginData = mapper.mapRequestToUserLoginModel(req.query);
+    //let isLoggingInPassed = await userLoginUseCase.userLogginIn(userLoginData);
 
-    res.send(isLoggingInPassed);
+    res.send('isLoggingInPassed');
 }
 
 async function register(req, res, next){
