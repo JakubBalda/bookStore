ab6e794bc37a44775e23470eb8452fc4f1007fbc
Jakub Balda <balda.jakub00@gmail.com>
2023-08-10 00:05:00 +0200

    Created update user data use case

---
+++ UsersAPI/services/updateUserDataUseCase.js
@@ -0,0 +1,11 @@
+const logger = require('../middleware/logger');
+const userMapper = require('../middleware/userMapper');
+const usersRepository = require('../repositories/usersRepository');
+const booleanFunctions = require('../utils/booleanFunctions');
+const validate = require('../utils/dataValidator');
+
+async function updateData(userData, userId){
+
+}
+
+module.exports = {updateData};
