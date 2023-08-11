85b5e5c7a6ff10ce28414ebe3e307530447051b8
Jakub Balda <balda.jakub00@gmail.com>
2023-08-11 23:40:55 +0200

    Deleted unused imports

--- UsersAPI/services/updateUserDataUseCase.js
+++ UsersAPI/services/updateUserDataUseCase.js
@@ -1,8 +1,5 @@
 const logger = require('../middleware/logger');
-const userMapper = require('../middleware/userMapper');
 const usersRepository = require('../repositories/usersRepository');
-const booleanFunctions = require('../utils/booleanFunctions');
-const validate = require('../utils/dataValidator');
 
 async function updateData(userData, userId){
     return usersRepository.updateUserDetails(userData, userId);
