6ee1614fe7cc3b2933ef72914169abb8d23880ef
Jakub Balda <balda.jakub00@gmail.com>
2023-08-21 10:07:40 +0200

    Edited delete book response information

--- BooksAPI/services/deleteBookUseCase.js
+++ BooksAPI/services/deleteBookUseCase.js
@@ -1,14 +1,13 @@
 const repository = require('../repositories/booksRepository');
-const booksMapper = require('../middleware/booksMapper');
 const logger = require('../middleware/logger');
 
 function deleteBook(bookID){
     logger.logInformation('deleteBookUseCase.deleteBook requested');
 
     if(repository.deleteBookById(bookID)){
-        return "Książka usunięta poprawnie!";
+        return "Deleted";
     }else{
-        return "Wystąpił błąd, książka nie została usunięta!";
+        return "Error";
     }
 }
 
