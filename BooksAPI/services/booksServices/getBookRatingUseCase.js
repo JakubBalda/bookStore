22e5359bbda72018835c16159823967b0f8f7adb
Jakub Balda <balda.jakub00@gmail.com>
2023-09-05 13:57:03 +0200

    Fixed getting user rating if user havent rated yet

--- BooksAPI/services/booksServices/getBookRatingUseCase.js
+++ BooksAPI/services/booksServices/getBookRatingUseCase.js
@@ -5,8 +5,13 @@ async function getRatingByUserId(userId, bookId){
     logger.logInformation('getBookRatingUseCase.getRatingByUserId requested');
 
     let rating = await bookRepository.getBookRatingByUserId(userId, bookId);
+    if(rating.length === 0){
+        return {Rating: 0}
+    }else{
+        return rating[0]
+    }
     
-    return rating[0] === undefined ? 0 : rating[0];
+    return rating[0] === [] ? 0 : rating[0];
 }
 
 module.exports = {getRatingByUserId};
