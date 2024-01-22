714ce9d3f0f92fdcc5251ce21c41508868dd8c5c
Jakub Bałda <balda.jakub00@gmail.com>
2024-01-22 12:52:03 +0100

    Fixed: checking if bookImage is uploaded

--- BooksAPI/controllers/booksController.js
+++ BooksAPI/controllers/booksController.js
@@ -29,17 +29,18 @@ const authorsMapper = require('../middleware/authorsMapper');
 
     async function storeNewBook(req, res, next){
         logRequest('storeNewBook', req);
-        if(req.files === null)
+        if(req.files === null){
             res.send('Nie wybrano zdjęcia okładki');
-
-        const fileBuffer = req.files.bookImage.data;
+        }else{
+            const fileBuffer = req.files.bookImage.data;
         
-        let author = authorsMapper.mapRequestToAuthorToStoreModel(req.body);
-        let book = booksMapper.mapRequestToBookToStoreModel(req.body);
-
-        let information = await storeNewBookUseCase.storeNewBook(author, book, fileBuffer);
-
-        res.send(information);
+            let author = authorsMapper.mapRequestToAuthorToStoreModel(req.body);
+            let book = booksMapper.mapRequestToBookToStoreModel(req.body);
+    
+            let information = await storeNewBookUseCase.storeNewBook(author, book, fileBuffer);
+    
+            res.send(information);
+        }
     }
 
     async function deleteBook(req, res, next){
